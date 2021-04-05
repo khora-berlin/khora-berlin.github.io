33bd82b840d07256b10f32595a251372c1da66dc
Achim Schneider <achim.schneider@posteo.de>
2021-04-05 15:53:07 +0200

    fixed khora header

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,63 +1,59 @@
-import { Link } from "gatsby"
-import * as React from "react"
-import styled from 'styled-components';
-import { ReactFitty } from 'react-fitty';
+import { Link } from 'gatsby'
+import * as React from 'react'
+import styled from 'styled-components'
+import { ReactFitty } from 'react-fitty'
 
 const KhoraHeader = styled.header`
-  background: #FC3638;
-  color: white;
-  margin-bottom: 1.45rem;
-  text-align: center;
-`;
+	background: #fc3638;
+	color: white;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
 
-const KhoraTitle = styled.div`
-  font-family: 'ArcadeClassic';
-  width: 50%;
-	text-align-last: justify;
+const KhoraTitle = styled.h1`
+	font-family: 'ArcadeClassic';
+	width: 50%;
+	margin-bottom: 0;
 `
 
 const KhoraSubTitle = styled.div`
-  font-size: 0.7em;
-  width: 50%;
-  font-family: 'ArcadeClassic';
+	width: 50%;
+	font-family: 'ArcadeClassic';
 `
 
 const Text = styled(ReactFitty)`
-  letter-spacing: 0.1em;
-  padding-right: -1em;
+	letter-spacing: 0.5em;
+	direction: rtl;
+	text-indent: -0.5em;
+	line-height: 1em;
 `
 
 const SubText = styled(ReactFitty)`
-  letter-spacing: 0.05em;
+	letter-spacing: 0.05em;
+	word-spacing: 0.1em;
+	font-size: 0.5em;
 `
 
-const Header = (props: HeaderProps) => (
-  <KhoraHeader>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <KhoraTitle>
-          <Text>{props.siteTitle}</Text>
-        </KhoraTitle>
-        <KhoraSubTitle>
-          <SubText>YOUR FAVOURITE FOOD DELIVERY COLLECTIVE</SubText>
-        </KhoraSubTitle>
-      </h1>
-    </div>
-  </KhoraHeader>
-)
+const Header = (props: HeaderProps) => {
+	return (
+		<KhoraHeader>
+			<KhoraTitle>
+				<Text>{props.siteTitle}</Text>
+			</KhoraTitle>
+			<KhoraSubTitle>
+				<SubText>YOUR FAVOURITE FOOD DELIVERY COLLECTIVE</SubText>
+			</KhoraSubTitle>
+		</KhoraHeader>
+	)
+}
 
 interface HeaderProps {
-  siteTitle?: string
+	siteTitle?: string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+	siteTitle: ``,
 }
 
 export default Header
