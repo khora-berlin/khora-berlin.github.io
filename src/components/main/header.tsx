1fa3c9f67e2830cc84905ba1f75711bfb2aed9d6
Achim Schneider <achim.schneider@posteo.de>
2021-04-06 08:28:35 +0200

    fixed repsonsiveness for iPhone4

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
-import styled from 'styled-components'
-import { ReactFitty } from 'react-fitty'
+import * as React from 'react';
+import styled from 'styled-components';
+import { ReactFitty } from 'react-fitty';
 
 const KhoraHeader = styled.header`
 	background: #fc3638;
@@ -9,11 +9,14 @@ const KhoraHeader = styled.header`
 	flex-direction: column;
 	align-items: center;
 	padding: 1em 0;
-`
+`;
 
 const KhoraTitle = styled.div`
 	font-family: 'ArcadeClassic';
-	@media only screen and (max-width: 600px) {
+	@media only screen and (max-width: 500px) {
+		width: 85%;
+	}
+	@media only screen and (min-width: 600px) {
 		width: 80%;
 	}
 	@media only screen and (min-width: 600px) {
@@ -23,25 +26,25 @@ const KhoraTitle = styled.div`
 		width: 40%;
 	}
 	margin-bottom: 0;
-`
+`;
 
 const Text = styled(ReactFitty)`
 	letter-spacing: 0.5em;
 	direction: rtl;
 	text-indent: -0.5em;
 	line-height: 1em;
-`
+`;
 
 const Symbol = styled.div`
 	font-family: 'KhoraSymbols';
 	display: inline;
-`
+`;
 
 const SubText = styled(ReactFitty)`
 	letter-spacing: 0.05em;
 	word-spacing: 0.1em;
 	font-size: 0.5em;
-`
+`;
 
 const Header = (props: HeaderProps) => {
 	return (
@@ -53,15 +56,15 @@ const Header = (props: HeaderProps) => {
 				<SubText>YOUR FAVOURITE FOOD DELIVERY COLLECTIVE</SubText>
 			</KhoraTitle>
 		</KhoraHeader>
-	)
-}
+	);
+};
 
 interface HeaderProps {
-	siteTitle?: string
+	siteTitle?: string;
 }
 
 Header.defaultProps = {
 	siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
