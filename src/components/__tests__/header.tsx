d2a0002713e8113bfc8a221949789eed059392cb
Achim Schneider <achim.schneider@posteo.de>
2021-04-08 07:57:21 +0200

    tried to fix testing with jest, still issues

--- src/components/__tests__/header.tsx
+++ src/components/__tests__/header.tsx
@@ -1,13 +1,19 @@
-import * as React from "react"
-import * as renderer from "react-test-renderer"
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
 
-import Header from "../layout/header"
+import Header from '../layout/header';
+import GlobalStyle from '../layout/global';
 
-describe("Header", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-			.create(<Header siteSubTitle="Your favourite food delivery collective" />)
+describe('Header', () => {
+	it('renders correctly', () => {
+		const tree = renderer
+			.create(
+				<>
+					<GlobalStyle />
+					<Header siteSubTitle="Your favourite food delivery collective" />
+				</>
+			)
 			.toJSON();
-    expect(tree).toMatchSnapshot()
-  })
-})
+		expect(tree).toMatchSnapshot();
+	});
+});
