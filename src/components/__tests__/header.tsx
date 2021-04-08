9ac4c54197ce59c33485ea20f3717bb86bd1e507
Achim Schneider <achim.schneider@posteo.de>
2021-04-08 08:05:49 +0200

    changes on test setup, still broken

--- src/components/__tests__/header.tsx
+++ src/components/__tests__/header.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
 import Header from '../layout/header';
 import GlobalStyle from '../layout/global';
+
+const createNodeMock = (el: any) => {
+	if (el.type === 'div') {
+		const mockEl = document.createElement('div');
+		mockEl.style.padding = '0';
+		Object.defineProperty(mockEl, 'clientHeight', {
+			get() {
+				return 100;
+			},
+			enumerable: true,
+			configurable: true,
+		});
+		Object.defineProperty(mockEl, 'clientWidth', {
+			get() {
+				return 300;
+			},
+			enumerable: true,
+			configurable: true,
+		});
+		return mockEl;
+	}
+	return null;
+};
 
 describe('Header', () => {
 	it('renders correctly', () => {
@@ -11,7 +35,8 @@ describe('Header', () => {
 				<>
 					<GlobalStyle />
 					<Header siteSubTitle="Your favourite food delivery collective" />
-				</>
+				</>,
+				{ createNodeMock }
 			)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
