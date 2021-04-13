77812675e4e57c6a6c3824fa94dc561031585f16
Achim Schneider <achim.schneider@posteo.de>
2021-04-13 08:43:02 +0200

    fixed gallery

--- src/components/instagram/gallery.tsx
+++ src/components/instagram/gallery.tsx
@@ -1,66 +1,64 @@
 /**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
+ * Gallery component that queries data from the Khora Insta Account
  */
 
 import * as React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './header';
-import styled from 'styled-components';
-import GlobalStyle from './global';
-import Footer from './footer';
-
-const SiteContent = styled.div`
-	background: var(--main-bg-color);
-	color: var(--main-fg-color);
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	gap: var(--main-gap);
-	min-height: 100vh;
-	max-width: 800px;
-	margin: 0 auto;
-`;
-
-const Main = styled.main`
-	height: 100%;
-	display: flex;
-	flex: 1;
-	flex-direction: column;
-	justify-content: space-between;
-	text-align: center;
-`;
-
-const Layout = (props: LayoutProps) => (
+const Gallery = (props: GalleryProps) => (
 	<StaticQuery
 		query={graphql`
-			query SiteTitleQuery {
-				site {
-					siteMetadata {
-						title
-						subTitle
+		query {
+			allInstaNode {
+			  edges {
+				node {
+				  id
+				  likes
+				  comments
+				  mediaType
+				  preview
+				  original
+				  timestamp
+				  caption
+				  hashtags
+				  localFile {
+					childImageSharp {
+					  fixed(width: 150, height: 150) {
+						...GatsbyImageSharpFixed
+					  }
 					}
+				  },
+				  permalink,
+				  carouselImages {
+					preview,
+					localFile {
+					childImageSharp {
+					  fixed(width: 150, height: 150) {
+						...GatsbyImageSharpFixed
+					  }
+					}
+				  },
+				  # Only available with the public api scraper
+				  thumbnails {
+					src
+					config_width
+					config_height
+				  }
+				  dimensions {
+					height
+					width
+				  }
 				}
+			  }
 			}
+		  }
 		`}
-		render={data => (
-			<>
-				<GlobalStyle />
-				<SiteContent>
-					<Header siteSubTitle={data.site.siteMetadata.subTitle} />
-					<Main>{props.children}</Main>
-					<Footer />
-				</SiteContent>
-			</>
-		)}
+		render={data => <></>}
 	/>
 );
 
-interface LayoutProps {
+interface GalleryProps {
 	children: any;
 }
 
-export default Layout;
+export default Gallery;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -8,60 +8,59 @@
 import * as React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
-const Gallery = (props: GalleryProps) => (
+import Header from './header';
+import styled from 'styled-components';
+import GlobalStyle from './global';
+import Footer from './footer';
+
+const SiteContent = styled.div`
+	background: var(--main-bg-color);
+	color: var(--main-fg-color);
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	gap: var(--main-gap);
+	min-height: 100vh;
+	max-width: 800px;
+	margin: 0 auto;
+`;
+
+const Main = styled.main`
+	height: 100%;
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	justify-content: space-between;
+	text-align: center;
+`;
+
+const Layout = (props: LayoutProps) => (
 	<StaticQuery
 		query={graphql`
-		query {
-			allInstaNode {
-			  edges {
-				node {
-				  id
-				  likes
-				  comments
-				  mediaType
-				  preview
-				  original
-				  timestamp
-				  caption
-				  hashtags
-				  localFile {
-					childImageSharp {
-					  fixed(width: 150, height: 150) {
-						...GatsbyImageSharpFixed
-					  }
+			query SiteTitleQuery {
+				site {
+					siteMetadata {
+						title
+						subTitle
 					}
-				  },
-				  permalink,
-				  carouselImages {
-					preview,
-					localFile {
-					childImageSharp {
-					  fixed(width: 150, height: 150) {
-						...GatsbyImageSharpFixed
-					  }
-					}
-				  },
-				  # Only available with the public api scraper
-				  thumbnails {
-					src
-					config_width
-					config_height
-				  }
-				  dimensions {
-					height
-					width
-				  }
 				}
-			  }
 			}
-		  }
 		`}
-		render={data => <></>}
+		render={data => (
+			<>
+				<GlobalStyle />
+				<SiteContent>
+					<Header siteSubTitle={data.site.siteMetadata.subTitle} />
+					<Main>{props.children}</Main>
+					<Footer />
+				</SiteContent>
+			</>
+		)}
 	/>
 );
 
-interface GalleryProps {
+interface LayoutProps {
 	children: any;
 }
 
-export default Gallery;
+export default Layout;
