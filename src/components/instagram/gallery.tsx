8e1ae92a509cdc6d0188a24cd218bef2a7cfb547
Achim Schneider <achim.schneider@posteo.de>
2021-04-13 08:56:17 +0200

    fixed gallery

--- src/components/instagram/gallery.tsx
+++ src/components/instagram/gallery.tsx
@@ -5,57 +5,55 @@
 import * as React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
-const Gallery = (props: GalleryProps) => (
-	<StaticQuery
-		query={graphql`
-		query {
-			allInstaNode {
-			  edges {
-				node {
-				  id
-				  likes
-				  comments
-				  mediaType
-				  preview
-				  original
-				  timestamp
-				  caption
-				  hashtags
-				  localFile {
-					childImageSharp {
-					  fixed(width: 150, height: 150) {
-						...GatsbyImageSharpFixed
-					  }
-					}
-				  },
-				  permalink,
-				  carouselImages {
-					preview,
-					localFile {
-					childImageSharp {
-					  fixed(width: 150, height: 150) {
-						...GatsbyImageSharpFixed
-					  }
-					}
-				  },
-				  # Only available with the public api scraper
-				  thumbnails {
-					src
-					config_width
-					config_height
-				  }
-				  dimensions {
-					height
-					width
+const Gallery = (props: GalleryProps) => {
+	const query = graphql`
+	query {
+		allInstaNode {
+		  edges {
+			node {
+			  id
+			  likes
+			  comments
+			  mediaType
+			  preview
+			  original
+			  timestamp
+			  caption
+			  hashtags
+			  localFile {
+				childImageSharp {
+				  fixed(width: 150, height: 150) {
+					...GatsbyImageSharpFixed
 				  }
 				}
+			  },
+			  permalink,
+			  carouselImages {
+				preview,
+				localFile {
+				childImageSharp {
+				  fixed(width: 150, height: 150) {
+					...GatsbyImageSharpFixed
+				  }
+				}
+			  },
+			  # Only available with the public api scraper
+			  thumbnails {
+				src
+				config_width
+				config_height
+			  }
+			  dimensions {
+				height
+				width
 			  }
 			}
 		  }
-		`}
-		render={data => <></>}
-	/>
-);
+		}
+	  }
+	`;
+	return <></>;
+};
 
 interface GalleryProps {
 	children: any;
