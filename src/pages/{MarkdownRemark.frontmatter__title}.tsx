577f7bfe50a5590a164088db0dd6cff9eec2038a
tuxfy <coder.tux@posteo.de>
2021-07-03 17:11:53 +0200

    aligns text of subpages to left

--- src/pages/{MarkdownRemark.frontmatter__title}.tsx
+++ src/pages/{MarkdownRemark.frontmatter__title}.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout/layout';
 import OrderButton from '../components/button/orderButton';
+import styled from 'styled-components';
 
 interface IPage {
-  data: {
+	data: {
 		content: {
-      html: string;
-      frontmatter: {
-        title: string
-      };
+			html: string;
+			frontmatter: {
+				title: string;
+			};
 		};
 	};
 }
@@ -25,18 +26,37 @@ export const query = graphql`
 	}
 `;
 
+const LeftAligned = styled.div`
+	@media only screen and (max-width: 500px) {
+		width: 85%;
+	}
+	@media only screen and (min-width: 500px) {
+		width: 80%;
+	}
+	@media only screen and (min-width: 600px) {
+		width: 60%;
+	}
+	@media only screen and (min-width: 1200px) {
+		width: 40%;
+	}
+	margin: 0 auto 20px auto;
+	text-align: left;
+	p {
+		margin: 0;
+	}
+`;
+
 const Page = ({ data }: IPage) => {
 	return (
 		<Layout>
-			<article
-				dangerouslySetInnerHTML={{
-					__html: data.content.html,
-				}}
-			></article>
+				<LeftAligned
+					dangerouslySetInnerHTML={{
+						__html: data.content.html,
+					}}
+				></LeftAligned>
 			{data.content.frontmatter.title == 'contact' && <OrderButton />}
 		</Layout>
 	);
 };
 
 export default Page;
-
