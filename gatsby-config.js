af534bea60ef594fc344913f160ab9bf2a3bddd4
achimcc <achim@parity.io>
2022-04-14 12:22:22 +0200

    add gatsby-plugin-image to config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
 		`gatsby-transformer-remark`,
 		`gatsby-plugin-typescript`,
 		`gatsby-plugin-styled-components`,
+		'gatsby-plugin-image',
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
